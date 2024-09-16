08cb5d77ca1e0d44be032c5b2933969443ac0a2c
Caio Timoteo <caiocesar.wk@gmail.com>
2024-09-16 17:48:44 -0300

    fetch

---
+++ fetch/fetch.js
@@ -0,0 +1,28 @@
+// console.log('ex0 - Fetch');
+
+// const url = 'https://ranekapi.origamid.dev/json/api/produto';
+
+// const getData = fetch(url);
+
+// getData.then((data) => {
+//   data
+//     .json()
+//     .then((jsonData) => {
+//       console.log(jsonData);
+//     })
+//     .catch((e) => {
+//       console.log('erro', e);
+//     });
+// });
+
+async function getData(url) {
+  try {
+    const dataFetch = await fetch(url);
+    const dataJson = await dataFetch.json();
+    console.log(dataJson);
+  } catch (error) {
+    console.log('EEEEERRRROR:', error);
+  }
+}
+
+getData('https://ranekapi.origamid.dev/json/api/produto');
